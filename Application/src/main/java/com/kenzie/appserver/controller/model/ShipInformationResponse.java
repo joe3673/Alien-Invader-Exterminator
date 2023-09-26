44c47c157dad98d021f781c543785e8ef353f2b7
HoldenBourg <holden.bourg@mykenzie.snhu.edu>
2023-09-25 18:29:19 -0700

    Fixed Models

--- Application/src/main/java/com/kenzie/appserver/controller/model/ShipInformationResponse.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/ShipInformationResponse.java
@@ -9,23 +9,26 @@ import javax.validation.constraints.NotEmpty;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ShipInformationResponse {
 
-
+    @NotEmpty
     @JsonProperty("id")
     private String gameId;
 
+    @NotEmpty
     @JsonProperty("playerCoordinates")
     private String playerCoordinates;
-
+    @NotEmpty
     @JsonProperty("alienCoordinates")
     private String alienCoordinates;
 
+    @Min(0)
     @JsonProperty("playerHealth")
     private int playerHealth;
-
     @Min(0)
     @JsonProperty("alienHealth")
     private int alienHealth;
 
+
+    // TODO Getters/Setters
     public String getGameId() {
         return gameId;
     }
@@ -33,7 +36,6 @@ public class ShipInformationResponse {
     public String getPlayerCoordinates() {
         return playerCoordinates;
     }
-
     public void setPlayerCoordinates(String playerCoordinates) {
         this.playerCoordinates = playerCoordinates;
     }
@@ -41,7 +43,6 @@ public class ShipInformationResponse {
     public String getAlienCoordinates() {
         return alienCoordinates;
     }
-
     public void setAlienCoordinates(String alienCoordinates) {
         this.alienCoordinates = alienCoordinates;
     }
@@ -49,7 +50,6 @@ public class ShipInformationResponse {
     public int getPlayerHealth() {
         return playerHealth;
     }
-
     public void setPlayerHealth(int playerHealth) {
         this.playerHealth = playerHealth;
     }
@@ -57,7 +57,7 @@ public class ShipInformationResponse {
     public int getAlienHealth() {
         return alienHealth;
     }
-
     public void setAlienHealth(int alienHealth) {
         this.alienHealth = alienHealth;
     }
+}
--- Application/src/main/java/com/kenzie/appserver/controller/model/ShipInformationUpdateRequest.java
+++ Application/src/main/java/com/kenzie/appserver/controller/model/ShipInformationUpdateRequest.java
@@ -8,6 +8,10 @@ import javax.validation.constraints.NotEmpty;
 public class ShipInformationUpdateRequest {
 
     @NotEmpty
+    @JsonProperty("id")
+    private String gameId;
+
+    @NotEmpty
     @JsonProperty("playerCoordinates")
     private String playerCoordinates;
     @NotEmpty
@@ -17,17 +21,19 @@ public class ShipInformationUpdateRequest {
     @Min(0)
     @JsonProperty("playerHealth")
     private int playerHealth;
-
     @Min(0)
     @JsonProperty("alienHealth")
     private int alienHealth;
 
 
+    // TODO Getters/Setters
+    public String getGameId() {
+        return gameId;
+    }
 
     public String getPlayerCoordinates() {
         return playerCoordinates;
     }
-
     public void setPlayerCoordinates(String playerCoordinates) {
         this.playerCoordinates = playerCoordinates;
     }
@@ -35,7 +41,6 @@ public class ShipInformationUpdateRequest {
     public String getAlienCoordinates() {
         return alienCoordinates;
     }
-
     public void setAlienCoordinates(String alienCoordinates) {
         this.alienCoordinates = alienCoordinates;
     }
@@ -43,7 +48,6 @@ public class ShipInformationUpdateRequest {
     public int getPlayerHealth() {
         return playerHealth;
     }
-
     public void setPlayerHealth(int playerHealth) {
         this.playerHealth = playerHealth;
     }
@@ -51,7 +55,6 @@ public class ShipInformationUpdateRequest {
     public int getAlienHealth() {
         return alienHealth;
     }
-
     public void setAlienHealth(int alienHealth) {
         this.alienHealth = alienHealth;
     }
